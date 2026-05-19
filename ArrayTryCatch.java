382935fc86969dd921a7fb38af164ee3c723eb28
Pavan Rathod <pavanrathod7856@gmail.com>
2026-05-19 23:03:33 +0530

    Added array try catch program

---
+++ ArrayTryCatch.java
@@ -0,0 +1,20 @@
+public class ArrayTryCatch {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30 };
+
+        try {
+
+            System.out.println(arr[5]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Array index does not exist");
+
+        }
+
+        System.out.println("Program ended");
+
+    }
+}
